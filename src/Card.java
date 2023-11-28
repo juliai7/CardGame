ebfa5333d592327d9591ef3db06485a21035794a
juliaim <julia.im@menloschool.org>
2023-11-28 13:45:17 -0800

    Created all files, finished Card class, started Deck class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Card {
+import java.util.Scanner;
+public class Card {
+    private String suit;
+    private char rank;
+    private int point;
+
+    public Card (String otherSuit, char otherRank, int otherPoint) {
+        suit = otherSuit;
+        rank = otherRank;
+        point = otherPoint;
+    }
+    public String getSuit() {
+        return suit;
+    }
+    public char getRank() {
+        return rank;
+    }
+    public int getPoint() {
+        return point;
+    }
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    public void setRank(char rank) {
+        this.rank = rank;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    public String toString() {
+        return rank " of " suit;
+    }
 }
---
+++ src/Deck.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Deck {
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(int[] point, char[] rank, String[] suit) {
+
+    }
+}
